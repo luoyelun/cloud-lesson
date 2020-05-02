66edaa16c4bb25fbe66ac9157e0ecb9dec076190
luoyelun <luoyelun@outlook.com>
2020-05-02 16:14:09 +0800

    创建项目

---
+++ src/test/java/icu/thyself/cloudlesson/CloudLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.thyself.cloudlesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
